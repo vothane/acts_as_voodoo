cc834182de44ae112460263a70ccdc0956ea23f4
Thane Vo <vothane@gmail.com>
2012-11-18 02:32:44 -0700

    added the first of regression tests before refactoring

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,29 @@
+require 'pry'
+require 'webmock/rspec'
+require 'timecop'
+require 'factory_girl'
+require 'vcr'
+
 $:.unshift(File.join(File.dirname(__FILE__), "..", "lib"))
 
 require 'acts_as_voodoo'
 
-def fixture_path
-  File.expand_path("../fixtures", __FILE__)
+RSpec.configure do |config|
+  # Use color in STDOUT
+  config.color_enabled = true
+
+  # Use color not only in STDOUT but also in pagers and files
+  config.tty = true
+
+  # Use the specified formatter
+  config.formatter = :documentation # :progress, :html, :textmate  
+
+  config.mock_with :rspec  
+
+  config.extend VCR::RSpec::Macros
 end
 
-def fixture(file)
-  File.new(fixture_path + '/' + file)
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/support/vcr_cassettes'
+  c.stub_with :webmock
 end
