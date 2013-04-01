90233c1a921a73d0353c5272d733e093359168a4
Thane Vo <vothane@gmail.com>
2013-04-01 15:45:40 -0600

    refactored tests for assets

--- spec/assets_spec.rb
+++ spec/assets_spec.rb
@@ -19,47 +19,74 @@ describe 'acts_as_voodoo for assets' do
     Timecop.return
   end
 
-  it "should create a new channel" do
-    VCR.use_cassette('create_channel') do
+  context "when assets are videos" do
+    context "when updating an attrbute of an existing video" do
+      
+      let(:video) do
+        VCR.use_cassette('find_video') do
+          results = Asset.find(:one) do |vid|
+            vid.description == "Thor"
+            vid.duration > 600
+          end
+          results.first
+        end
+      end
+
+      it "should update the name attribute through ooyala api" do
+        video.name = "Hear Me Roar!"
+
+        VCR.use_cassette('update_video') do
+          video.save.should be_true
+        end
+      end
+    end
+    
+    context "when checking to see that the name attribute was actually updated" do 
+      
+      let(:video_with_updated_name) do
+        VCR.use_cassette('find_video_with_updated_name') do
+          results = Asset.find(:one) do |vid|
+            vid.description == "Thor"
+            vid.duration > 600
+          end
+          results.first
+        end
+      end
+
+      it "should have updated the name attribute on ooyala servers" do
+        video_with_updated_name.name.should == "Hear Me Roar!" 
+      end  
+    end
+
+    context "when deleting an existing video" do 
+
+      let(:video_to_be_deleted) do  
+        VCR.use_cassette('find_video_to_delete') do
+          results = Asset.find(:one) do |vid|
+            vid.description == "Mongoid"
+            vid.duration > 600
+          end
+          results.first
+        end
+      end
+
+      it "should delete an existing video" do
+        VCR.use_cassette('delete_video') do
+          video_to_be_deleted.destroy.should be_true
+        end
+      end
+    end  
+  end
+
+  context "when assets are channels" do
+    it "should create a new channel" do
       new_channel            = Asset.new
       new_channel.asset_type = "channel"
       new_channel.name       = "new channel"
-      new_channel.save.should be_true
-    end
-  end
 
-  it "should update an existing video" do
-    video = nil
-
-    VCR.use_cassette('find_video') do
-      results = Asset.find(:one) do |vid|
-        vid.description == "Thor"
-        vid.duration > 600
+      VCR.use_cassette('create_channel') do
+        new_channel.save.should be_true
       end
-
-      video = results.first
     end
-
-    VCR.use_cassette('update_video') do
-      video.name = "update name"
-      video.save.should be_true
-    end
-  end
-
-  it "should delete a video" do
-    video = nil
-
-    VCR.use_cassette('find_video_to_delete') do
-      results = Asset.find(:one) do |vid|
-        vid.description == "Bootstrap"
-        vid.duration > 600
-      end
-
-      video = results.first
-    end
-
-    VCR.use_cassette('delete_video') do
-      video.destroy.should be_true
-    end
-  end
+  end  
 end 
