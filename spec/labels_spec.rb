9ef403569791872a87823b6778cc39bdeae7e772
Thane Vo <vothane@gmail.com>
2013-04-02 15:33:10 -0600

    refactor tests for labels

--- spec/labels_spec.rb
+++ spec/labels_spec.rb
@@ -1,6 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe 'acts_as_voodoo for assets' do
+describe 'acts_as_voodoo for labels' do
 
   class Label < ActiveResource::Base
      my_api_key    = 'JkN2w61tDmKgPl4y395Rp1vAdlcq.IqBgb'
@@ -12,30 +12,50 @@ describe 'acts_as_voodoo for assets' do
   end
 
   before :all do
-    Timecop.freeze(Time.local(2013, 1, 1, 10, 0, 0))
+    Timecop.freeze(Time.local(2014, 1, 1, 10, 0, 0))
   end
 
   after :all do
     Timecop.return
   end
 
-  it "should create a new label" do
-    VCR.use_cassette('create_label') do
-      new_label = Label.new
-      new_label.name = "new label"
-      new_label.save.should be_true
-      labels = Label.find(:all)
-      labels.collect { |label| label.name }.should include( "new label" ) 
+  context "when labels" do
+    context "when saving new labels" do
+      let(:new_label) do
+        label      = Label.new
+        label.name = "new label"
+        label
+      end
+
+      it "should create a new label" do
+        VCR.use_cassette('create_label') do
+          new_label.save.should be_true
+        end
+      end
     end
-  end 
 
-  it "should destroy label" do
-    VCR.use_cassette('destroy_label') do
-      labels = Label.find(:all)
+    context "when finding that label that was just saved and then destroy it" do
+      let(:labels) do
+        VCR.use_cassette('find_all_labels') do
+          labels = Label.find(:all)
+        end
+      end
 
-      labels.each do |label|
-        if label.name == "new label"
-          label.destroy.should be_true
+      let(:label_to_be_destroyed) do
+        labels.each do |label|
+          if label.name == "new label"
+            return label
+          end
+        end
+      end
+
+      it "should find newly created label" do
+        labels.collect { |label| label.name }.should include( "new label" )
+      end
+
+      it "should destroy newly created label" do
+        VCR.use_cassette('destroy_label') do
+          label_to_be_destroyed.destroy.should be_true
         end
       end
     end
