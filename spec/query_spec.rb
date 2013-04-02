dc1808980cd77aa25ebe4ad755ec972e5e676998
Thane Vo <vothane@gmail.com>
2013-04-02 16:33:20 -0600

    refactor tests for asset querys

--- spec/query_spec.rb
+++ spec/query_spec.rb
@@ -12,7 +12,7 @@ describe 'acts_as_voodoo for querying assets' do
   end
 
   before :all do
-    Timecop.freeze(Time.local(2013, 1, 1, 10, 0, 0))
+    Timecop.freeze(Time.local(2014, 1, 1, 10, 0, 0))
   end
 
   after :all do
@@ -20,70 +20,75 @@ describe 'acts_as_voodoo for querying assets' do
   end
 
   it "should find every video in ooyala account" do
+    results = nil
     VCR.use_cassette('query_all') do
       results = Asset.find(:all) 
-      results.count.should == 15
     end
+    results.count.should == 14
   end 
 
-  it "should correctly query Avengers trailer video by substring in description text" do
+  it "should correctly query a video by substring in description text" do
+    results = nil
     VCR.use_cassette('query_by_description') do
       results = Asset.find(:all) do |vid|
                   vid.description == "Iron Man, Thor, Captain America, and the Hulk"
                 end
-      results.count.should == 1
-      results.first.name.should == "Avengers"
     end
+    results.count.should == 1
+    results.first.name.should == "Avengers"
   end
 
   it "should correctly query by embed code" do
+    results = nil
     VCR.use_cassette('query_by_embed_code"') do
       results = Asset.find(:all) do |vid|
                   vid.embed_code * "('U3NmdxMzrJe_3B_8VLs1ZlrlIJfSID-9','g1YzBnMjrEWdqX0gNdtKwTwQREhEkf9e')"
                 end
-      results.count.should == 1
-      results.first.name.should == "Iron Sky"
     end
+    results.count.should == 1
+    results.first.name.should == "Iron Sky"
   end
 
   it "should correctly query by label" do
+    results = nil
     VCR.use_cassette('query_by_label') do
       results = Asset.find(:all) do |vid|
                   vid.labels =~ "TV Commercial"
-                end
-      results.count.should == 1
-      results.first.name.should == "The Force Volkswagen Commercial"
+                end  
     end
+    results.count.should == 1
+    results.first.name.should == "The Force Volkswagen Commercial"
   end
 
-  # TODO: FIX
-  # query by duration does not work
-  xit "should correctly query a video by duration" do
-    VCR.use_cassette('query_avengers_by_duration') do
+  it "should correctly query a video by duration" do
+    results = nil
+    VCR.use_cassette('query_by_duration') do
       results = Asset.find(:all) do |vid|
-                  vid.duration > 1500
+                  vid.duration < 600
                 end
-      results.count.should == 1
     end
+    results.count.should == 1
   end  
 
   it "should correctly query by union of criterias, or joining with AND" do
+    results = nil
     VCR.use_cassette('query_by_union') do
       results = Asset.find(:all) do |vid|
                   vid.description == "Thor"
                   vid.labels =~ "Movie Trailer"
                 end     
-      results.count.should == 2
     end
+    results.count.should == 2
   end 
 
   it "should find none when a criteria is FALSEY in union of criterias" do
+    results = nil
     VCR.use_cassette('query_by_union_but_falsey') do
       results = Asset.find(:all) do |vid|
                   vid.description == "Thor"
                   vid.labels =~ "TV Commercial"
                 end     
-      results.count.should == 0
     end
+    results.count.should == 0
   end        
 end 
