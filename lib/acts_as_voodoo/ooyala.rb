2f966785ab3a5c53a0850e6acc8b02116caf0e30
Thane Vo <vothane@gmail.com>
2013-04-18 18:02:31 -0600

    extract conditionals to Ooyala module

--- lib/acts_as_voodoo/ooyala.rb
+++ lib/acts_as_voodoo/ooyala.rb
@@ -17,4 +17,33 @@ module OOYALA
     expiration = Time.now.to_i + expiration_window
     expiration + ROUND_UP_TIME - (expiration%ROUND_UP_TIME)
   end
+
+  def self.find_params(*args, asset, &block)
+    scope        = args.slice!(0)
+    scope        = :all if scope.instance_of? Integer
+    options      = args.slice!(0)
+    path         = "/v2/#{asset.collection_name}"
+    path         = "#{path}/#{scope}" if scope.instance_of? String
+    this_params  = { 'api_key' => asset.api_key, 'expires' => OOYALA::expires }
+
+    if block_given?
+       conditions = Query::Conditions.new(&block)
+
+       this_params.merge(options) if options.instance_of? Hash
+       this_params['where']     = conditions.to_where_conditions
+       this_params['signature'] = OOYALA::generate_signature(self.api_secret, "GET", path, this_params, nil)
+       return this_params
+    else   
+      if options && options[:from]
+        this_params['signature'] = OOYALA::generate_signature( asset.api_secret, "GET", "#{path}#{options[:from]}", this_params)
+        return this_params
+      elsif options
+        this_params['signature'] = OOYALA::generate_signature( asset.api_secret, "GET", path, this_params)
+        return this_params
+      else
+        this_params['signature'] = OOYALA::generate_signature( asset.api_secret, "GET", path, this_params)
+        return this_params
+      end
+    end
+  end  
 end
